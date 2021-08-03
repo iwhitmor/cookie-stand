06a3a3e14b4210850e6419dd8db7533440791a47
Ian Whitmore <ianwhitmor@gmail.com>
2021-08-03 17:10:29 -0500

    delete objectliterals js file and add constructor function for all locations

--- js/app.js
+++ js/app.js
@@ -32,8 +32,53 @@ function custPerHour(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+Location.prototype.render = function () {
+  let parentElement = document.getElementById('salesData');
+  console.log(parentElement);
+  let article = document.createElement('article');
+  console.log('article');
+  parentElement.appendChild(article);
+  let h2 = document.createElement('h2');
+  h2.setAttribute('class', 'storeName');
+  h2.textContent = this.storeName;
+  console.log(h2);
+  article.appendChild(h2);
+  let cookiesUl = document.createElement('ul');
+  cookiesUl.setAttribute('class', 'cookiesPerHour');
+  article.appendChild(cookiesUl);
+  for (let i = 0; i < this.cookiesPerHour.length; i++) {
+    let cookiesLi = document.createElement('li');
+    console.log(cookiesLi);
+    cookiesLi.textContent = `${hours[i]}: ${this.cookiesPerHour[i]}`;
+    cookiesUl.appendChild(cookiesLi);
+  }
+  let totalCookiesLi = document.createElement('li');
+  totalCookiesLi.textContent = `Total: ${this.totalDailyCookies}`;
+  cookiesUl.appendChild(totalCookiesLi);
+};
+
 let locationOne = new Location('Seattle', 23, 65, 6.3, [], [], 0);
 locationOne.aveCustPerHour();
 locationOne.aveCookiesPerHour();
 
+let locationTwo = new Location('Tokyo', 3, 24, 1.2, [], [], 0);
+locationTwo.aveCustPerHour();
+locationTwo.aveCookiesPerHour();
 
+let locationThree = new Location('Dubai', 11, 38, 3.7, [], [], 0);
+locationThree.aveCustPerHour();
+locationThree.aveCookiesPerHour();
+
+let locationFour = new Location('Paris', 20, 38, 2.3, [], [], 0);
+locationFour.aveCustPerHour();
+locationFour.aveCookiesPerHour();
+
+let locationFive = new Location('Lima', 2, 16, 4.6, [], [], 0);
+locationFive.aveCustPerHour();
+locationFive.aveCookiesPerHour();
+
+let locations = [locationOne, locationTwo, locationThree, locationFour, locationFive];
+
+for (let i = 0; i < locations.length; i++) {
+  locations[i].render();
+}
